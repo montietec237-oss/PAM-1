7026b7318ce932bedd1b4e845ceaf625f5bc7463
montietec237-oss <montietec.237@gmail.com>
2026-05-08 12:09:00 +0000

    final

--- projeto/componentes/TelaInicial/index.js
+++ projeto/componentes/TelaInicial/index.js
@@ -8,3 +8,22 @@ export default function TelaInicial() {
     );
 
 }
+
+import React from 'react';
+import { View, Button, Image, Text } from 'react-native';
+import logo from '../../assets/logo.jpg';
+import styles from '../styles';
+
+export default function TelaInicial( props ) {
+    return (
+        <View style={styles.container}>
+            <Text>Os três filmes mais famosos da Barbie</Text>
+            <Image source={logo} style={styles.imagem } />
+
+            <Text style={styles.titulo}>Você sabe quais são os três mais famosos da Barbie?</Text>
+
+            <Text style={styles.texto}>Para saber os filmes mais famosos, clique no botão abaixo para ver:</Text>
+
+        </View>
+    );
+}
